84d067757d9b1eb94d609aa8232980c1ea2bf76a
stringnine <alexjee85@gmail.com>
2025-10-05 21:44:36 +0900

    [week3/mission] 3주차 미션3 제출

--- week-3_mission-2/src/types/movie.ts
+++ week-3_mission-2/src/types/movie.ts
@@ -21,3 +21,95 @@ export type MovieResponse = {
     total_pages: number;
     total_results: number;
 }
+
+export type Genre = {
+    id: number;
+    name: string;
+}
+
+export type ProductionCompany = {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+}
+
+export type ProductionCountry = {
+    iso_3166_1: string;
+    name: string;
+}
+
+export type SpokenLanguage = {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+}
+
+export type MovieDetail = {
+    genres: Genre[];
+    adult: boolean;
+    backdrop_path: string | null;
+    belongs_to_collection: {
+        id: number;
+        name: string;
+        poster_path: string | null;
+        backdrop_path: string | null;
+    }
+    budget: number;
+    homepage: string | null;
+    id: number;
+    imdb_id: string;
+    origin_country: string[];
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    production_companies: ProductionCompany[];
+    production_countries: ProductionCountry[];
+    spoken_languages:SpokenLanguage[];
+}
+
+export type Cast = {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+}
+
+export type Crew = {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    credit_id: string;
+    department: string;
+    job: string;
+}
+
+export type Credits = {
+    id: number;
+    cast: Cast[];
+    crew: Crew[];
+}
