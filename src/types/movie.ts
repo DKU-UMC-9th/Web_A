3ec67aec9fff2686d23cb185c619dbff8f1d40a5
sismp3476 <sismp3476@naver.com>
2025-10-07 16:06:38 +0900

    [쑤기} 3주차 미션 1

---
+++ src/types/movie.ts
@@ -0,0 +1,18 @@
+export type Movie = {
+    adult: boolean;
+    backdrop_path: string;
+    first_air_date: string;
+    genre_ids: number[];
+    id: number;
+    name: string;
+    origin_country: string[];
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    vote_average: number;
+    vote_count: number;
+    title: string;
+    release_date: string;
+};
