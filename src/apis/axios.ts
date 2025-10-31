503b91e1664b5c7d486d494e2188c7a9d69dfe6c
“ninininhihi” <nasasm1120@naver.com>
2025-11-01 00:00:39 +0900

    week5 mission

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,3 +2,22 @@ import axios from "axios";
 import { LOCAL_STORAGE_KEY } from "../constants/key";
 
 export const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_SERVER_API_URL,
+})
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const tokenString = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+        if (tokenString) {
+            try {
+                const token = JSON.parse(tokenString);
+                config.headers.Authorization = `Bearer ${token}`;
+            } catch {
+                config.headers.Authorization = `Bearer ${tokenString}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,19 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getMyInfo } from "../apis/auth";
+import type { ResponseMyInfoDTO } from "../types/auth";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-export default function MyPage() {
+const MyPage = () => {
+
+    const navigate = useNavigate();
+
+    const {logout} = useAuth();
+
+    const [data, setData] = useState<ResponseMyInfoDTO>([]);
     useEffect(() => {
         const getData = async () => {
             const response = await getMyInfo();
             console.log(response);
-        };
+
+            setData(response);
+        }; 
 
         getData();
     },[]);
 
-    return(
-        <div>
-            MyPage
+    const handleLogout = async() => {
+        await logout();
+        navigate("/");
+    }
+
+
+    console.log(data.data?.name);
+
+    return <div>
+        <h1>{data.data?.name}님 환영합니다.</h1>
+        <img src={data.data?.avatar as string} alt="프로필 이미지" />
+        <h1>{data.data?.email}</h1>
+
+        <button
+        className="cursor-pointer bg-red-500 text-white px-4 py-2 rounded-md mt-4" 
+        onClick={handleLogout}>로그아웃</button>
         </div>
-    )
 }
+
+export default MyPage;
