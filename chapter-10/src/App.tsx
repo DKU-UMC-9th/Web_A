35f4cb5761cee30829fbaf4d9283304b6879526f
stringnine <alexjee85@gmail.com>
2025-12-23 21:27:10 +0900

    [week10] 10주차 미션2 제출

--- chapter-10/src/App.tsx
+++ chapter-10/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MovieDetailPage from './pages/MovieDetailPage'; // 새로 만든 페이지 import
 import './App.css'
-import UseCallbackPage from './useCallbackPage'
 
 function App() {
-
   return (
-    <>
-      <UseCallbackPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
